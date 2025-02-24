1a734b42f18ecba0b7541e0eabeef22b609fbcac
Марина Печегина <marinapechegina@yandex.ru>
2025-02-24 19:16:49 +0700

    подправила 1 дз, попыталась сделать 2 дз

--- lecture01hw.py
+++ lecture01hw.py
@@ -1,7 +1,6 @@
-print("Enter 1st side: ")
-b = input()
-print("Enter a 2nd side: ")
-c = input()
-print("Enter angle(degrees) between 1st and 2nd side: ")
-x = input()
-print("3rd side =", sqrt(b**2+c**2-2*b*c*cos(radians(x))))
+import math
+
+first_side = input("Enter 1st side: ")
+second_side = input("Enter a 2nd side: ")
+angle_degrees = input("Enter angle(degrees) between 1st and 2nd side: ")
+print("3rd side =", math.sqrt(b**2+c**2-2*b*c*math.cos(math.radians(x))))
