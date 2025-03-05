bae2a875ca19cda71a2363db2ce0b5af89de366c
Марина Печегина <marinapechegina@yandex.ru>
2025-03-05 10:49:02 +0700

    дз с переводом шкал

---
+++ lecture03hw.py
@@ -0,0 +1,12 @@
+temp_str = input("Введите температуру (например, 25C или 77F): ")
+temperature = float(temp_str[:-1])  # Извлекаем числовое значение
+scale = temp_str[-1]  # Извлекаем символ шкалы (C или F)
+if scale not in ('C', 'F'):
+  print("Ошибка: Некорректная шкала. Используйте C для Цельсия или F для Фаренгейта.")
+# Конвертируем из Цельсия в Фаренгейта
+if scale == 'C':
+  fahrenheit = (temperature * 9/5) + 32
+  print(f"{temperature:.1f}C = {fahrenheit:.1f}F")
+if scale == 'F': 
+  celsius = (temperature - 32) * 5/9 
+  print(f"{temperature:.1f}F = {celsius:.1f}C")
